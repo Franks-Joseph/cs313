90743d2312ad6e3de906e89fc52b18b8434967a5
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-07 00:02:46 -0400

    upgraded index.php and index.css

--- web/index.php
+++ web/index.php
@@ -18,19 +18,38 @@
       <div class="bio-container">
         <h3 style="text-align:center">A little about me...</h3>
         <hr>
-        <p>I like tinkering with tech and coming up with new ideas.</p>
-        <p>I like to get into video games but I like the computers that you can build to run them. I really like the hardware that is behind all of that.</p>
+        <p>I like to be with my family.</p>
+        <p>I enjoy computers and programming. I like to play with my son and do things together. It's important to me that Family remains as one of the most important things to take care of.</p>
       </div>
       <div class="pic-container">
-        <img src="https://i.imgur.com/H6h9fFp.jpg" style="width:104px;height:142px;">
+        <div class="profile-picture">
+          <img src="https://i.imgur.com/H6h9fFp.jpg">
+        </div>
       </div>
       <div class="server-timer-container">
-        <?php
+        <div id="timestamp">
+          <?php
           	date_default_timezone_set('America/New_York');
     		echo $timestamp = date('h:i:s');
         ?>
-        <div id="timestamp"></div>
+        </div>
       </div>
+      <div></div>
+      <div></div>
+      <div class="Week"><a href="#">Week1</a></div>
+      <div class="Week"><a href="#">Week2</a></div>
+      <div class="Week"><a href="#">Week3</a></div>
+      <div class="Week"><a href="#">Week4</a></div>
+      <div class="Week"><a href="#">Week5</a></div>
+      <div class="Week"><a href="#">Week6</a></div>
+      <div class="Week"><a href="#">Week7</a></div>
+      <div class="Week"><a href="#">Week8</a></div>
+      <div class="Week"><a href="#">Week9</a></div>
+      <div class="Week"><a href="#">Week10</a></div>
+      <div class="Week"><a href="#">Week11</a></div>
+      <div class="Week"><a href="#">Week12</a></div>
+      <div class="Week"><a href="#">Week13</a></div>
+      <div class="Week"><a href="#">Week14</a></div>
     </div>
   </body>
 </html>
