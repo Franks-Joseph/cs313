d5d8eacbbe9c2b6628ea3c732e8f0574c36cbe8b
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-24 01:16:51 -0400

    update cart.php

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -8,6 +8,8 @@ $db = connect_db();
    <head>
      <meta charset="utf-8">
      <title>Cart</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+     <link rel="stylesheet" href="cart.css">
    </head>
    <body>
      <div>
