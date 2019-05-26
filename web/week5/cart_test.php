74c1f81e748ea05e00b734aeda48106236109944
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-26 00:14:33 -0400

    update

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -24,14 +24,14 @@ $db = connect_db();
                 ?>
                 <!-- Creates a responsive grid layout using bootstrap -->
                 <div class="col-sm-4 col-md-3">
-                  <form method="post" action="index.php?action=add&id=<?php echo $product['id']; ?>">
+                  <form method="post" action="index.php?action=add&id=<?php echo $row['id']; ?>">
                     <!-- Here we are going to put the items down that are in the products table in a nice way. -->
-                    <img src="<?php echo "./" . $product['image']; ?>" class="img-responsive">
-                    <h4 class="text-info"><?php echo $product['name']; ?></h4>
-                    <h4>$ <?php echo $product['price']; ?></h4>
+                    <img src="<?php echo "./" . $row['image']; ?>" class="img-responsive">
+                    <h4 class="text-info"><?php echo $row['name']; ?></h4>
+                    <h4>$ <?php echo $row['price']; ?></h4>
                     <input type="text" name="quantity" class="form-control" value="1">
-                    <input type="hidden" name="name" value="<?php echo $product['name']; ?>">
-                    <input type="hidden" name="price" value="<?php echo $product['price']; ?>">
+                    <input type="hidden" name="name" value="<?php echo $row['name']; ?>">
+                    <input type="hidden" name="price" value="<?php echo $row['price']; ?>">
                     <input type="submit" name="add-to-cart" style="margin-top: 7px;" class="btn btn-info" value="Add to Cart">
                   </form>
                 </div>
