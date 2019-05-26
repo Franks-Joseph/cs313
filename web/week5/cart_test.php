1151b7cebfcdd0612cf677d58cba42d5c988249a
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-25 22:17:10 -0400

    update

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -18,7 +18,6 @@ $db = connect_db();
        <?php
          $statement = $db->prepare("SELECT name, image, price FROM products");
           $statement->execute();
-          if ($statement){
             if (pg_num_rows($statement)>0){
               while ($row = $statement->fetch(PDO::FETCH_ASSOC))
               {
@@ -40,7 +39,6 @@ $db = connect_db();
                 <?php
               }
             }
-          }
         ?>
      </div>
    </body>
