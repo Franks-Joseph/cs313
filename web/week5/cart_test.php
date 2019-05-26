5293ec58dce4ed151d2d06c54ee573c71af4a24a
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-26 00:34:22 -0400

    update

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -8,7 +8,7 @@ $db = connect_db();
    <head>
      <meta charset="utf-8">
      <title>Cart</title>
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
      <link rel="stylesheet" href="cart.css">
    </head>
    <body>
