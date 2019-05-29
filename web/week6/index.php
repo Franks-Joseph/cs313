01a0e286139071db6e6be80d6397e15c1a1be4be
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-28 23:57:51 -0400

    update

--- web/week6/index.php
+++ web/week6/index.php
@@ -159,7 +159,60 @@ function test_input($data) {
                         ?>
 					</div>
 					<div class="tab-pane container fade" id="core3">
-core 3
+
+                        <table id="example" class="table table-striped table-bordered" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>Book</th>
+                                    <th>Chapter</th>
+                                    <th>Verse</th>
+                                    <th>Content</th>
+                                    <th>Topic</th>
+                                </tr>
+                            </thead>
+                            <?php
+                            $statement = $db->prepare(
+                                "SELECT
+                                    book,
+                                    chapter,
+                                    verse,
+                                    content,
+                                    name
+                                FROM scripture
+                                    INNER JOIN lookup ON scripture=scripture.id
+                                    INNER JOIN topic ON topic=topic.id");
+
+                            $statement->execute();
+                            // Go through each result
+                            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                            {
+                                // The variable "row" now holds the complete record for that
+                                // row, and we can access the different values based on their
+                                // name
+                                $book = $row['book'];
+                                $chapter = $row['chapter'];
+                                $verse = $row['verse'];
+                                $content = $row['content'];
+                                $topic_name = $row['name'];
+                                echo '<tr>';
+                                
+                                    echo '<td>'.$book.'</td>';
+                                    echo '<td>'.$chapter.'</td>';
+                                    echo '<td>'.$verse.'</td>';
+                                    echo '<td>'.$content.'</td>';
+                                    echo '<td>'.$topic_name.'</td>';
+                                echo '</tr>';
+                                //echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+                            }
+                            ?>
+                        </table>
+                            <script>
+                                $(document).ready(function() {
+                                    $('#example').DataTable();
+                                } );
+                            </script>
+
+
 					</div>
 					
 					
