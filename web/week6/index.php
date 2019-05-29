71a27e6fb3d521068b2aeca5ed2f27500e5dbbff
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-05-28 23:52:16 -0400

    update

--- web/week6/index.php
+++ web/week6/index.php
@@ -83,83 +83,61 @@ function test_input($data) {
 						core 1 code...
 					</div>
 					<div class="tab-pane container fade" id="core2">
-                        <form action="index.php" method="POST">
-                            <?php // Core 2 ?>
-                            <h3>Insert a new scripture</h3>
-                            <hr />
-                            <label>Book</label>
-                            <br />
-                            <input type="text" name="book" id="book" /><br />
-                            <br />
-                            <label>Chapter</label>
-                            <br />
-                            <input type="text" name="chapter" id="chapter" /><br />
-                            <label>Verse</label>
-                            <br />
-                            <input type="text" name="verse" id="verse" /><br />
-                            <hr />
-                            <label>Content</label>
-                            <br />
-                            <textarea rows="4" cols="50" name="content" id="content">
-                                <?php // Content ?>
-                            </textarea>
-                            <br /><br />
+					</div>
+					<div class="tab-pane container fade" id="core3">
+
+                        <table id="example" class="table table-striped table-bordered" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>Book</th>
+                                    <th>Chapter</th>
+                                    <th>Verse</th>
+                                    <th>Content</th>
+                                    <th>Topic</th>
+                                </tr>
+                            </thead>
                             <?php
-                            $statement = $db->prepare("SELECT id, name FROM topic");
+                            $statement = $db->prepare(
+                                "SELECT
+                                    book,
+                                    chapter,
+                                    verse,
+                                    content,
+                                    name
+                                FROM scripture
+                                    INNER JOIN lookup ON scripture=scripture.id
+                                    INNER JOIN topic ON topic=topic.id");
+
                             $statement->execute();
                             // Go through each result
                             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                             {
-                                $topic_id = $row['id'];
+                                // The variable "row" now holds the complete record for that
+                                // row, and we can access the different values based on their
+                                // name
+                                $book = $row['book'];
+                                $chapter = $row['chapter'];
+                                $verse = $row['verse'];
+                                $content = $row['content'];
                                 $topic_name = $row['name'];
-                                echo '<input type="checkbox" name="topic_id" value="'.$topic_id.'" >'.$topic_name . '';
+                                echo '<tr>';
+                                
+                                    echo '<td>'.$book.'</td>';
+                                    echo '<td>'.$chapter.'</td>';
+                                    echo '<td>'.$verse.'</td>';
+                                    echo '<td>'.$content.'</td>';
+                                    echo '<td>'.$topic_name.'</td>';
+                                echo '</tr>';
+                                //echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
                             }
                             ?>
-                            <br /><br />
-                            <hr />
-                            <input type="submit" class="btn btn-info" value="Submit Button" id="update">
-                        </form>
+                            </table>
+                            <script>
+                                $(document).ready(function() {
+                                    $('#example').DataTable();
+                                } );
+                            </script>
 
-                        <?php
-                        
-                        
-                        $book = test_input($_POST['book']);
-                        $chapter = test_input($_POST['chapter']);
-                        $verse = test_input($_POST['verse']);
-                        $content = test_input($_POST['content']);
-                        $topics = test_input($_POST['topic_id']);
-                                             
-                        $statement = $db->prepare("INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)");
-                       
-                        
-
-                        $statement->bindValue(':book', $book);
-                        $statement->bindValue(':chapter', $chapter);
-                        $statement->bindValue(':verse', $verse);
-                        $statement->bindValue(':content', $content);
-                        
-
-                        $statement->execute();
-
-                        $newId = $pdo->lastInsertId('scripture_id_seq');
-                        foreach ($topics as $topic_id)
-                        {
-                            echo 'Adding ' . $topic_id . ' for '. $newid . '<br />';
-                            $statement = $db->prepare("INSERT INTO lookup(scripture,topic) VALUES(:script_id, :topic_id)");
-                            
-                            //Bind
-                            $statement->bind(':script_id',$newId);
-                            $statement->bind(':topic_id',$topic_id);
-
-                            $statement->execute();
-
-                        }
-
-                        
-                        ?>
-					</div>
-					<div class="tab-pane container fade" id="core3">
-                                    Core 3
 
 					</div>
 					
