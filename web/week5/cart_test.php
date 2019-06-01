0dc971c89f3999b8717d38521d75d560a6bb99fb
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-01 19:27:10 -0400

    update

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -12,7 +12,7 @@ $db = connect_db();
      <link rel="stylesheet" href="cart.css">
    </head>
    <body>
-     <div class="container">
+     <div class="container d-inline">
        <?php
          $statement = $db->prepare("SELECT name, image, price FROM products");
           $statement->execute();
