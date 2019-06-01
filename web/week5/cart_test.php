9f53096a13c7574a349099949cfcd28223c1eb57
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-01 19:37:30 -0400

    update

--- web/week5/cart_test.php
+++ web/week5/cart_test.php
@@ -13,30 +13,32 @@ $db = connect_db();
    </head>
    <body>
      <div class="container">
-       <?php
-         $statement = $db->prepare("SELECT name, image, price FROM products");
-          $statement->execute();
-              while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-              {
-                ?>
-                <!-- Creates a responsive grid layout using bootstrap -->
-                <div class="d-inline col-sm-4 col-md-3">
-                  <form method="post" action="index.php?action=add&id=<?php echo $row['id']; ?>">
-                    <div class="products">
-                      <!-- Here we are going to put the items down that are in the products table in a nice way. -->
-                      <img src="<?php echo "./" . $row['image']; ?>" class="img-responsive" style="width:100%;">
-                      <h4 class="text-info"><?php echo $row['name']; ?></h4>
-                      <h4>$ <?php echo $row['price']; ?></h4>
-                      <input type="text" name="quantity" class="form-control" value="1">
-                      <input type="hidden" name="name" value="<?php echo $row['name']; ?>">
-                      <input type="hidden" name="price" value="<?php echo $row['price']; ?>">
-                      <input type="submit" name="add-to-cart" style="margin-top: 7px;" class="btn btn-info" value="Add to Cart">
+       <div class="row">
+           <?php
+              $statement = $db->prepare("SELECT name, image, price FROM products");
+              $statement->execute();
+                  while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                  {
+                    ?>
+                    <!-- Creates a responsive grid layout using bootstrap -->
+                    <div class="d-inline col-sm-4 col-md-3">
+                      <form method="post" action="index.php?action=add&id=<?php echo $row['id']; ?>">
+                        <div class="products">
+                          <!-- Here we are going to put the items down that are in the products table in a nice way. -->
+                          <img src="<?php echo "./" . $row['image']; ?>" class="img-responsive" style="width:100%;">
+                          <h4 class="text-info"><?php echo $row['name']; ?></h4>
+                          <h4>$ <?php echo $row['price']; ?></h4>
+                          <input type="text" name="quantity" class="form-control" value="1">
+                          <input type="hidden" name="name" value="<?php echo $row['name']; ?>">
+                          <input type="hidden" name="price" value="<?php echo $row['price']; ?>">
+                          <input type="submit" name="add-to-cart" style="margin-top: 7px;" class="btn btn-info" value="Add to Cart">
+                        </div>
+                      </form>
                     </div>
-                  </form>
-                </div>
-                <?php
-              }
-        ?>
+                    <?php
+                  }
+            ?>
+       </div>
      </div>
    </body>
  </html>
