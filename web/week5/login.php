1028fdd8436ad4c5ff159794e06c886d9fdbb6b0
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-02 03:12:41 -0400

    update

--- web/week5/login.php
+++ web/week5/login.php
@@ -3,26 +3,37 @@ include_once('header.php');
 include_once('link.php');
  ?>
 
-<div id="frmRegistration">
-<form class="form-horizontal" method="POST" action="login_code.php">
-	<h1>User Login</h1>
+<div class="container login-container">
+    <div class="row">
+      <div class="col-md-6 login-form-1">
+        <h1>User Login</h1>
+        <form class="form-horizontal" method="POST" action="login_code.php">
 
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Email:</label>
-    <div class="col-sm-6">
-      <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
+          <div class="form-group"> <!-- Email -->
+            <label class="control-label col-sm-2" for="email">Email:</label>
+            <div class="col-sm-6">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
+            </div>
+          </div>
+
+          <div class="form-group"> <!-- Password -->
+            <label class="control-label col-sm-2" for="pwd">Password:</label>
+            <div class="col-sm-6">
+              <input type="password" class="form-control" name="password" id="pwd" placeholder="Enter password">
+            </div>
+          </div>
+
+          <div class="form-group"> <!-- Login -->
+            <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" name="login" class="btn btn-primary">Login</button>
+            </div>
+          </div>
+
+          <div class="form-group"> <!-- Forget Password? -->
+            <a href="#" class="ForgetPwd">Forget Password?</a>
+          </div>
+
+        </form>
+      </div>
     </div>
-  </div>
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="pwd">Password:</label>
-    <div class="col-sm-6">
-      <input type="password" class="form-control" name="password" id="pwd" placeholder="Enter password">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" name="login" class="btn btn-primary">Login</button>
-    </div>
-  </div>
-</form>
 </div>
