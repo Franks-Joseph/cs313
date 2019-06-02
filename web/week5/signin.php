0bd0c8247a93e0633c0bf8e35dbcc4eb69d4d072
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-02 03:43:38 -0400

    update

--- web/week5/signin.php
+++ web/week5/signin.php
@@ -8,11 +8,11 @@ $email = $_POST['email'];
 $pwd = $_POST['password'];
 $password = MD5($pwd);
 
-$statement = $db->prepare("SELECT * FROM user WHERE Email='$email' AND Password='$password'");
+$statement = $db->prepare("SELECT * FROM user WHERE email='$email' AND password='$password'");
 $statement->execute();
 	while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-		$id = $row["ID"];
-		$email = $row["Email"];
+		$id = $row["id"];
+		$email = $row["email"];
 		session_start();
 		$_SESSION['id'] = $id;
 		$_SESSION['email'] = $email;
