e8a0855af316091f8d59e6d88914069f6c4a9d5c
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-02 02:50:24 -0400

    update

---
+++ web/week5/header.php
@@ -0,0 +1,15 @@
+<?php
+include_once('link.php'); //This connects to the bootstrap and stuff.
+?>
+
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Registration Login</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="registration.php">Registration</a></li>
+			<li><a href="login.php">Login</a></li>
+		</ul>
+	</div>
+</nav>
---
+++ web/week5/link.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="login.css">
