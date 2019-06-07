4df6e978a5a86769fdc29318114b7abe6dbee4c6
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-07 15:40:18 -0400

    update

--- web/week7/registration.php
+++ web/week7/registration.php
@@ -7,22 +7,10 @@ include_once('link.php');
 <form method="POST" class="form-signin" action="login.php">
   <img class="mb-4" src="#" alt="" width="72" height="72">
 	<h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
-	<div class="form-group">
-    <label for="inputEmail" class="sr-only">Email</label>
-    <div class="col-sm-6">
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword" class="sr-only">Password</label>
-    <div class="col-sm-6">
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </div>
-  </div>
+	<label for="inputEmail" class="sr-only">Email</label>
+	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+	<label for="inputPassword" class="sr-only">Password</label>
+	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 </form>
 </body>
