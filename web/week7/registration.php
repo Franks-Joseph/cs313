a41ac09a5f799d03da9f11e1de635b6e92d7ce9b
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-07 15:28:06 -0400

    update

--- web/week7/registration.php
+++ web/week7/registration.php
@@ -3,25 +3,26 @@ include_once('header.php');
 include_once('link.php');
 ?>
 
-<div id="frmRegistration">
-<form method="POST" class="form-horizontal" action="login.php">
-	<h1>User Registration</h1>
-  <div class="form-group">
-    <label class="control-label col-sm-2" for="email">Email:</label>
+<body class="text-center">
+<form method="POST" class="form-signin" action="login.php">
+  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+	<h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
+	<div class="form-group">
+    <label for="inputEmail" class="sr-only">Email</label>
     <div class="col-sm-6">
-      <input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
     </div>
   </div>
   <div class="form-group">
-    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <label for="inputPassword" class="sr-only">Password</label>
     <div class="col-sm-6">
-      <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password">
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
     </div>
   </div>
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" name="create" class="btn btn-primary">Submit</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </div>
   </div>
 </form>
-</div>
+</body>
