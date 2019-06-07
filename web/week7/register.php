b700bbb4c292def36b2b6fa0fa39d6ca108a0634
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-06-07 17:01:45 -0400

    update

--- web/week7/register.php
+++ web/week7/register.php
@@ -1,49 +1,44 @@
 <?php
-require 'connect_db.php';
-$db = connect_db();
 
-//Client Side
-$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING)
-$password_clearText = filter_input(INPUT_POST, 'pwd', FILTER_SANITIZE_STRING)
+// When the user clicks "Sign in"
+$email = filter_input(INPUT_POST, 'inputEmail', FILTER_SANITIZE_STRING)
+$password = filter_input(INPUT_POST, 'inputPassword', FILTER_SANITIZE_STRING)
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (isset ($password_clearText)
-  && isset($username)) {
-    $rows = regClient($email, $password_clearText);
-
-    if ($row > 0)
-    {
-      header('Location: login.php');
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  if (isset ($email) || $email == "" || !isset($password) || $password == "")
+  {
+      header('Location: registration.php');
       die();
-    }
-    else {
+  }
+  else
+  {
       echo '<p>Error, try again!</p>';
-    }
   }
 }
 
+// Email
+$email = htmlspecialchars($email);
 
+// Hashed Password
+$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
-function regClient($email, $password_clearText) {
-  $sql = 'INSERT INTO user (email, password)
-          VALUES (:email, :password)';
+//Connect to db
+require 'connect_db.php';
+$db = connect_db();
 
-  // Server Side
-  $email = test_input($email);
-  $password_clearText = test_input($password_clearText);
+// Insert the values into the table
+$sql = 'INSERT INTO user(email, password)
+        VALUES (:email, :password)';
 
-  $password = password_hash($password_clearText, PASSWORD_DEFAULT);
+// Prepare, Bind, and Execute
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+$stmt-> execute();
 
-  $stmt = $needfunctionname->prepare($sql);
-  $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-  $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+header("Location: signIn.php");
+die();
 
-  $stmt-> execute();
-
-  // See if the rows changed
-  $rowsChanged = $stmt->rowCount();
-  $stmt->closeCursor();
-  return $rowsChanged;
-}
-
+// This is done.
 ?>
--- web/week7/registration.php
+++ web/week7/registration.php
@@ -1,15 +1,29 @@
 <?php
 include_once('link.php');
+include_once('register.php');
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+</head>
 <body class="text-center">
-<form method="POST" class="form-signin" action="login.php">
+<form method="POST" class="form-signin" action="register.php">
+
   <img class="mb-4" src="<?php echo "./" + $product['logo icon']; ?>" alt="" width="72" height="72">
+
 	<h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
+
 	<label for="inputEmail" class="sr-only">Email</label>
-	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+
+	<input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+
 	<label for="inputPassword" class="sr-only">Password</label>
-	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+	<input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required>
+
 	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 </form>
 </body>
+
+<!-- This should be done. -->
