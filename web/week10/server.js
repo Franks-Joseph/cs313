78d9e483ebb2b481950f831064396716240db5d5
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-07-11 22:31:53 -0400

    update

--- web/week10/server.js
+++ web/week10/server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var dotenv = require('dotenv');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 var server = app.listen(3000, () => {
  console.log('server is running on port', server.address().port);
 });
@@ -16,9 +16,9 @@ dotenv.config();
 // Variable for our MongoDB database URL.
 var db = process.env.MONGO_DB;
 
-io.on('db', () =>{
- console.log('a user is connected')
-})
+// io.on('db', () =>{
+//  console.log('a user is connected')
+// })
 
 // Connect to DB
 // mongoose.connect(db , { useNewUrlParser: true }) => {
@@ -45,12 +45,12 @@ app.get('/messages', (req, res) => {
 })
 
 // post new messages created by the user to the db
-app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  })
-})
+// app.post('/messages', (req, res) => {
+//   var message = new Message(req.body);
+//   message.save((err) =>{
+//     if(err)
+//       sendStatus(500);
+//     io.emit('message', req.body);
+//     res.sendStatus(200);
+//   })
+// })
