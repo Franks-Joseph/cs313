7fc98eacd9cebaa2ec51bc5499421a0f48f2d15a
H3XU55 <43921513+Franks-Joseph@users.noreply.github.com>
2019-07-17 16:36:08 -0400

    added the new node js project

--- web/week11/routes/index.js
+++ web/week11/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
-router.get('/', (req, res) => res.send('Hello World!'));
+// Welcome Page
+router.get('/', (req, res) => res.render('welcome'));
 
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 module.exports = router;
